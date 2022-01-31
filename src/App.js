138e2d8c0bce3b837b78d2025c6d7a6916b4b9cb
David Ortiz-Alejandre <ortiz.david00@gmail.com>
2022-01-31 13:45:03 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div className="Card">
+          <img src="david.jpg" className="App-img" alt="David Ortiz" />
+          <h1>David Ortiz-Alejandre</h1>
+          <h4>Frontend Developer</h4>
+          <div className="About">
+            <h3>About Me</h3>
+            <p>
+              I am a frontend developer with an interest in making web pages and
+              applications simple for users, while considering the mobile-first
+              approach for each task. I am constantly learning about best
+              practices and new languages/frameworks to expand my skills and
+              knowledge on current topics.
+            </p>
+            <footer className="Footer">
+              <a href="https://github.com/ortizdavid00">
+                <img
+                  src="https://img.icons8.com/ios-glyphs/45/000000/github.png"
+                  alt="GitHub"
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/david-ortiz-alejandre-676aa2183/">
+                <img
+                  src="https://img.icons8.com/ios-filled/45/000000/linkedin.png"
+                  alt="LinkedIn"
+                />
+              </a>
+              <a href="https://ortizdavid00.github.io/portfolio/">
+                <img
+                  src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/45/000000/external-portfolio-advertising-kiranshastry-solid-kiranshastry.png"
+                  alt="Portfolio"
+                />
+              </a>
+            </footer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
